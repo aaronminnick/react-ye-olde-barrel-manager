57c9749690d912a93f4e490165ea358cdb54d11b
Aaron Minnick <abminnick@gmail.com>
2022-02-11 10:09:45 -0800

    implement props correctly

--- src/components/Barrel.js
+++ src/components/Barrel.js
@@ -6,25 +6,23 @@ import DiscardButton from './DiscardButton';
 function Barrel(props) {
 
   const BarrelStyle = {
-    backgroundColor="blanchedalmond",
-    border="2px solid brown",
-    borderRadius="50%"
+    backgroundColor: "blanchedalmond",
+    border: "2px solid brown",
+    borderRadius: "50%"
   }
 
-  const {barrel} = props.barrel;
-
   let buttonToShow = null;
-  (barrel.tapped) ?
+  (props.barrel.tapped) ?
     buttonToShow = <PintButton /> :
     buttonToShow = <TapButton />;
 
   return (
     <React.Fragment>
       <div style={BarrelStyle}>
-        <h2>{barrel.brand} {barrel.name}</h2>
-        <h3>{barrel.price} shillings</h3>
-        <p><em>{barrel.alcoholContent}</em></p>
-        <p>{barrel.notes}</p>
+        <h2>{props.barrel.brand} {props.barrel.name}</h2>
+        <h3>{props.barrel.price} shillings</h3>
+        <p><em>{props.barrel.alcoholContent}</em></p>
+        <p>{props.barrel.notes}</p>
       </div>
       {buttonToShow}
       <DiscardButton />
